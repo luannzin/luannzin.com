9bd1fa9c065f5e6cd969d093fbc3944a5e1a8765
luannzin <luandaniel966@gmail.com>
2023-10-10 10:03:19 -0300

    fix(seo): add light theme logo

--- app/components/Logo/index.tsx
+++ app/components/Logo/index.tsx
@@ -42,40 +42,41 @@ const Logo = ({ theme }: LogoProps) => {
       );
 
     case "light":
-      return;
-      <svg
-        width="75"
-        height="75"
-        viewBox="0 0 75 75"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M5 5V69.9275"
-          stroke="black"
-          stroke-width="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M53.6956 26.3043L21.2319 69.8896"
-          stroke="black"
-          stroke-width="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M21.2319 26.3043L53.6957 26.3043"
-          stroke="black"
-          stroke-width="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M21.2319 69.9275H53.6957"
-          stroke="black"
-          stroke-width="10"
-          stroke-linecap="round"
-        />
-        <circle cx="69.9275" cy="69.9276" r="5.07246" fill="black" />
-      </svg>;
+      return (
+        <svg
+          width="75"
+          height="75"
+          viewBox="0 0 75 75"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M5 5V69.9275"
+            stroke="black"
+            stroke-width="10"
+            stroke-linecap="round"
+          />
+          <path
+            d="M53.6956 26.3043L21.2319 69.8896"
+            stroke="black"
+            stroke-width="10"
+            stroke-linecap="round"
+          />
+          <path
+            d="M21.2319 26.3043L53.6957 26.3043"
+            stroke="black"
+            stroke-width="10"
+            stroke-linecap="round"
+          />
+          <path
+            d="M21.2319 69.9275H53.6957"
+            stroke="black"
+            stroke-width="10"
+            stroke-linecap="round"
+          />
+          <circle cx="69.9275" cy="69.9276" r="5.07246" fill="black" />
+        </svg>
+      );
   }
 };
 
