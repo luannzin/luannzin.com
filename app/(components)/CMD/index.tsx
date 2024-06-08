ac30eede5c7b2683298b02e6708cced232b2263e
luannzin <luandaniel966@gmail.com>
2024-06-08 10:32:29 -0300

    feat: add new terminal template

---
+++ app/(components)/CMD/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+const CMD = () => {
+  return (
+    <div className="bg-zinc-800 rounded-lg border border-zinc-800 w-fit h-fit">
+      <header className="p-2 -mb-4">
+        <div className="bg-black rounded-lg px-4 py-2 text-sky-50 max-w-fit text-sm">
+          <strong>luannzin.com: chat</strong>
+        </div>
+      </header>
+      <div className="w-[1200px] h-[600px] bg-black rounded-lg"></div>
+      <main></main>
+    </div>
+  );
+};
+
+export default CMD;
--- app/(routes)/terminal/page.tsx
+++ app/(routes)/terminal/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CMD from "@/app/(components)/CMD";
 import Header from "@/app/(components)/Header";
 import { AnimatePresence, motion } from "framer-motion";
 
@@ -10,10 +11,10 @@ export default function Terminal() {
           opacity: 0,
           x: 100,
         }}
-        className="p-8 flex flex-col gap-4 max-w-[32rem] text-black dark:text-sky-50"
+        className="p-8 flex flex-col gap-4 max-w-[32rem]"
       >
         <Header />
-        <span>terminal</span>
+        <CMD />
       </motion.div>
     </AnimatePresence>
   );
