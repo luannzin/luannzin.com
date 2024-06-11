97ed82e57af31bfe0f8350d693f88300562de913
Luan Fabri <luandaniel966@gmail.com>
2024-06-11 08:53:32 -0300

    fix: change uuid localStorage logic

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,9 @@ export default function Home() {
 
   useEffect(() => {
     if (!uuid) {
+      if (localStorage.getItem("uuid")) {
+        return setUuid(localStorage.getItem("uuid"));
+      }
       const newUuid = uuidv4();
       setUuid(newUuid);
       localStorage.setItem("uuid", newUuid);
