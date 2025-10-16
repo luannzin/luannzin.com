4b9673a5ab98aaf5b68a4bc1082b31e59528beb5
luannzin <luandaniel966@gmail.com>
2025-10-15 23:54:06 -0300

    refactor: improve layout and styling on homepage, adjust responsiveness, and enhance link interactions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,36 +6,50 @@ import { cn } from "@/lib/utils";
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <div className="w-full h-20 border-b border-border flex items-center justify-center">
-        <div className="h-full max-w-[1920px] w-full border-x border-border"></div>
+      <div className="w-full h-20 lg:border-b border-border flex items-center justify-center lg:px-20">
+        <div className="h-full max-w-[1440px] w-full lg:border-x border-border"></div>
       </div>
-      <div className="h-full max-w-[1920px] w-full border-x border-border">
-        <div className="w-full h-full flex items-center justify-center">
-          <div className="flex flex-col gap-4">
-            <Logo />
-            <div className="flex flex-col gap-0">
-              <span>I'm Luan Fabri,</span>
-              <span className="text-muted-foreground">
-                a brazillian, gifted and handsome frontend developer.
-              </span>
-            </div>
-            <Link
-              href="/about"
-              className={cn("group text-primary block w-fit")}
-            >
-              <div className="relative flex flex-col">
+      <div className="w-full h-full lg:px-20 flex items-center justify-center">
+        <div className="h-full max-w-[1440px] w-full lg:border-x border-border max-lg:px-8">
+          <div className="w-full h-full flex items-center justify-center">
+            <div className="flex flex-col gap-4">
+              <Logo />
+              <div className="flex flex-col max-sm:gap-1">
+                <span className="max-sm:text-xl font-medium">
+                  I'm Luan Fabri,
+                </span>
+                <span className="text-muted-foreground">
+                  a brazillian, gifted and handsome frontend developer.
+                </span>
+              </div>
+              <Link
+                href="/about"
+                className={cn("group text-primary block w-fit max-sm:hidden")}
+              >
+                <div className="relative flex flex-col">
+                  <div className="flex items-center gap-1">
+                    <span>About</span>
+                    <ArrowRight className="absolute -right-5 size-4 group-hover:-rotate-45 blur-[4px] group-hover:blur-none scale-75 group-hover:scale-100 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out" />
+                  </div>
+                  <span className="absolute bottom-[2px] left-0 h-px w-full bg-primary origin-right scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100 group-hover:origin-left" />
+                </div>
+              </Link>
+              <Link
+                href="/about"
+                className={cn("sm:hidden text-primary underline")}
+              >
                 <div className="flex items-center gap-1">
                   <span>About</span>
-                  <ArrowRight className="absolute -right-5 size-4 group-hover:-rotate-45 blur-[4px] group-hover:blur-none scale-75 group-hover:scale-100 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out" />
+                  <ArrowRight className="size-4 -rotate-45" />
                 </div>
-                <span className="absolute bottom-[2px] left-0 h-px w-full bg-primary origin-right scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100 group-hover:origin-left" />
-              </div>
-            </Link>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-      <div className="w-full h-20 border-t border-border flex items-center justify-center">
-        <div className="h-full max-w-[1920px] w-full border-x border-border"></div>
+
+      <div className="w-full h-20 lg:border-t border-border flex items-center justify-center lg:px-20">
+        <div className="h-full max-w-[1440px] w-full lg:border-x border-border"></div>
       </div>
     </div>
   );
