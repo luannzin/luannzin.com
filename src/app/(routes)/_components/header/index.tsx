3d0284120c62edfe220e8e40cbb8198d9d9bcaa3
luannzin <luandaniel966@gmail.com>
2026-02-23 13:54:03 -0300

    feat: add next-themes dependency, create components.json, and update layout with theming support

---
+++ src/app/(routes)/_components/header/index.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="h-16 flex items-center justify-between">
+      <div className="flex flex-col w-full">
+        <p>luannzin</p>
+        <span>frontend developer</span>
+      </div>
+    </header>
+  );
+};
+
+export { Header };
