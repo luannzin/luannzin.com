de1f74d5fb42c38839186f1a692839cc13214a92
luannzin <luandaniel966@gmail.com>
2026-07-01 13:24:52 -0300

    refactor: update GitHub contributions fetching to use Next.js revalidation and tags for improved caching

--- src/lib/data/github.ts
+++ src/lib/data/github.ts
@@ -1,18 +1,17 @@
-import { cacheLife, cacheTag } from "next/cache";
 import { GITHUB_USERNAME as username } from "../config/constants";
 import { parseGithubContributions } from "../github/contributions/parse-html";
 
 export const github = {
   contributions: async () => {
-    "use cache";
-    cacheTag(username, "contributions");
-    cacheLife("days");
-
     const response = await fetch(
       `https://github.com/users/${username}/contributions`,
       {
         headers: {
           "User-Agent": "Mozilla/5.0",
+        },
+        next: {
+          revalidate: 60 * 60 * 24, // 1 day
+          tags: [username, "contributions"],
         },
       },
     );
